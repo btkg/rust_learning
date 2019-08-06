0ff67afc7bba7fc9c97bd24f6a0a781526e034f5
imagainstzy <zy119159@gmail.com>
2019-08-06 20:17:26 +0800

    cargo test

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/stuser/sourcecode/rust_learning/hello_cargo/target/debug/hello_cargo: /home/stuser/sourcecode/rust_learning/hello_cargo/src/main.rs
---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-5ec1f785bbb87a1e.d
@@ -0,0 +1,5 @@
+/home/stuser/sourcecode/rust_learning/hello_cargo/target/rls/debug/deps/hello_cargo-5ec1f785bbb87a1e.rmeta: src/main.rs
+
+/home/stuser/sourcecode/rust_learning/hello_cargo/target/rls/debug/deps/hello_cargo-5ec1f785bbb87a1e.d: src/main.rs
+
+src/main.rs:
