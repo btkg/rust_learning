aeaeb79350f18bc23296d965482485452f7c2e3a
imagainstzy <zy119159@gmail.com>
2019-08-07 09:19:35 +0800

    guessing game

---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/stuser/sourcecode/rust_learning/guessing_game/target/debug/guessing_game: /home/stuser/sourcecode/rust_learning/guessing_game/src/main.rs
---
+++ guessing_game/target/rls/debug/deps/guessing_game-440044e7136f64bd.d
@@ -0,0 +1,5 @@
+/home/stuser/sourcecode/rust_learning/guessing_game/target/rls/debug/deps/guessing_game-440044e7136f64bd.rmeta: src/main.rs
+
+/home/stuser/sourcecode/rust_learning/guessing_game/target/rls/debug/deps/guessing_game-440044e7136f64bd.d: src/main.rs
+
+src/main.rs:
